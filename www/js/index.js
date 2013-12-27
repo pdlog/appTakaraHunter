e959ac0e62cb4462189c503a8bc5ef6f7730e4bd
Elolawyn <ral4576@gmail.com>
2013-12-27 17:07:48 +0100

    cambiado un poco a jquery

--- www/js/index.js
+++ www/js/index.js
@@ -1,114 +1,93 @@
-// Declaración de variables globales
+// Declaraci√≥n de variables globales.
 var myScroll, myScrollMenu, cuerpo, menuprincipal, wrapper, estado;
 
-// Guardamos en variables elementos para poder rescatarlos después sin tener que volver a buscarlos
+// Guardamos en variables elementos para poder rescatarlos despu√©s sin tener que volver a buscarlos.
 cuerpo = document.getElementById("cuerpo"),
 menuprincipal = document.getElementById("menuprincipal"),
 wrapper = document.getElementById("wrapper");
 
 var xhReq = new XMLHttpRequest();
 
-var app = {
-    // Constructor de la app
-    initialize: function() {
-    	// Estado inicial mostrando capa cuerpo
-    	estado="cuerpo";
+// Constructor de la app.
+var app =
+{    
+    initialize: function()
+	{
+    	// Estado inicial mostrando capa cuerpo.
+    	estado = "cuerpo";
     	
-    	// Creamos el elemento style, lo añadimos al html y creamos la clase cssClass para aplicarsela al contenedor wrapper
-	    var heightCuerpo=window.innerHeight-46;
+    	// Creamos el elemento style, lo a√±adimos al html y creamos la clase cssClass para aplic√°rsela al contenedor wrapper.
+	    var heightCuerpo = window.innerHeight - 46;
 	    var style = document.createElement('style');
 	    style.type = 'text/css';
 	    style.innerHTML = '.cssClass { position:absolute; z-index:2; left:0; top:46px; width:100%; height: '+heightCuerpo+'px; overflow:auto;}';
 	    document.getElementsByTagName('head')[0].appendChild(style);
 	    
-	    // Añadimos las clases necesarias
+	    // A√±adimos las clases necesarias.
 		cuerpo.className = 'page center';
 		menuprincipal.className = 'page center';
 		wrapper.className = 'cssClass';
 			
-		// Leemos por ajax el archivos opcion1.html de la carpeta opciones
-		xhReq.open("GET", "opciones/index/opcion1.html", false);
-		xhReq.send(null);
-		document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
+		// Leemos por ajax el archivos opcion1.html de la carpeta opciones/index.
+		$("#contenidoCuerpo").load("opciones/index/opcion1.html");
 
-		// Leemos por ajax el archivos menu.html de la carpeta opciones
-		xhReq.open("GET", "opciones/index/menu.html", false);
-		xhReq.send(null);
-		document.getElementById("contenidoMenu").innerHTML=xhReq.responseText;
+		// Leemos por ajax el archivos menu.html de la carpeta opciones/index.
+		$("#contenidoMenu").load("opciones/index/menu.html");
 		
-		// Creamos los 2 scroll mediante el plugin iscroll, uno para el menú principal y otro para el cuerpo
+		// Creamos los 2 scroll mediante el plugin iscroll, uno para el men√∫ principal y otro para el cuerpo.
 		myScroll = new iScroll('wrapper', { hideScrollbar: true });
 		myScrollMenu = new iScroll('wrapperMenu', { hideScrollbar: true });
 	
         this.bindEvents();
     },
-
-    bindEvents: function() {
+    bindEvents: function()
+	{
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-
-    onDeviceReady: function() {
-    	// Ejecutamos la función FastClick, que es la que nos elimina esos 300ms de espera al hacer click
+    onDeviceReady: function()
+	{
+    	// Ejecutamos la funci√≥n FastClick, que es la que nos elimina esos 300ms de espera al hacer click.
     	new FastClick(document.body);
-    },
-    
+    }, 
 };
 
-// Función para añadir clases css a elementos
-function addClass( classname, element ) {
-    var cn = element.className;
-    if( cn.indexOf( classname ) != -1 ) {
-    	return;
-    }
-    if( cn != '' ) {
-    	classname = ' '+classname;
-    }
-    element.className = cn+classname;
-}
-
-// Función para eliminar clases css a elementos
-function removeClass( classname, element ) {
-    var cn = element.className;
-    var rxp = new RegExp( "\\s?\\b"+classname+"\\b", "g" );
-    cn = cn.replace( rxp, '' );
-    element.className = cn;
-}
-
-function menu(opcion){
-	
-	// Si pulsamos en el botón de "menu" entramos en el if
-	if(opcion=="menu"){
-		if(estado=="cuerpo"){
+function menu(opcion)
+{
+	// Si pulsamos en el bot√≥n de "menu" entramos en el if.
+	if(opcion == "menu")
+	{
+		if(estado == "cuerpo")
+		{
 			cuerpo.className = 'page transition right';
-			estado="menuprincipal";
-		}else if(estado=="menuprincipal"){
-			cuerpo.className = 'page transition center';
-			estado="cuerpo";	
+			estado = "menuprincipal";
 		}
-	// Si pulsamos un botón del menu principal entramos en el else
-	}else{
+		else if(estado == "menuprincipal")
+		{
+			cuerpo.className = 'page transition center';
+			estado = "cuerpo";
+		}
+	}
+	// Si pulsamos un bot√≥n del menu principal entramos en el else.
+	else
+	{
+		// A√±adimos la clase al li presionado.
+		$("#ulMenu li").eq(opcion).addClass("li-menu-activo");
 		
-		// Añadimos la clase al li presionado
-		addClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opcion]);
+		// Recogemos mediante ajax el contenido del html seg√∫n la opci√≥n clickeada en el men√∫.
+		$("#contenidoCuerpo").load("opciones/index/opcion"+opcion+".html");
 		
-		// Recogemos mediante ajax el contenido del html según la opción clickeada en el menu
-		xhReq.open("GET", "opciones/index/opcion"+opcion+".html", false);
-		xhReq.send(null);
-		document.getElementById("contenidoCuerpo").innerHTML=xhReq.responseText;
-		
-		// Refrescamos el elemento iscroll según el contenido ya añadido mediante ajax, y hacemos que se desplace al top
+		// Refrescamos el elemento iscroll seg≈ìn el contenido ya a√±adido mediante ajax, y hacemos que se desplace al top.
 		myScroll.refresh();
 		myScroll.scrollTo(0,0);
 		
-		// Añadimos las clases necesarias para que la capa cuerpo se mueva al centro de nuestra app y muestre el contenido
+		// A√±adimos las clases necesarias para que la capa cuerpo se mueva al centro de nuestra app y muestre el contenido.
 		cuerpo.className = 'page transition center';
-		estado="cuerpo";
+		estado = "cuerpo";
 		
-		// Quitamos la clase añadida al li que hemos presionado
-		setTimeout(function() {
-			removeClass('li-menu-activo' , document.getElementById("ulMenu").getElementsByTagName("li")[opcion]);
+		// Quitamos la clase a√±adida al li que hemos presionado.
+		setTimeout(function()
+		{
+			$("#ulMenu li").eq(opcion).removeClass("li-menu-activo");
 		}, 300);
-		 
-	 }
-
+	}
 }
