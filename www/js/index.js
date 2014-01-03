8f2585fe373755a45bc524b5543356f0778f4efd
Elolawyn <ral4576@gmail.com>
2014-01-03 17:23:48 +0100

    agregado entrar a búsqueda

--- www/js/index.js
+++ www/js/index.js
@@ -17,6 +17,13 @@
 }*/
 
 var he_hecho_login_antes = false; // PARA KE EL PERFIL SE VEA BIEN MODO WARRO NIVEL DIOS
+var he_entrado_en_lista_busquedas_antes = false; // PARA KE LAS BUSQUEDAS SE VEA BIEN MODO WARRO NIVEL DIOS
+var he_entrado_en_una_busqueda_antes = false; // PARA KE LA CAZA SE VEA BIEN MODO WARRO NIVEL DIOS
+
+var tesoro_posicion_x = 0;
+var tesoro_posicion_y = 0;
+var nombre_busqueda = "";
+var descripcion_busqueda = "";
 
 $(document).ready(function()
 {
