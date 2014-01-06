e53cde169039e7f20272c9761e3060346dd5530e
Elolawyn <ral4576@gmail.com>
2014-01-06 16:03:20 +0100

    agregado menu mas bonito para contentar a pedro :)

--- www/js/mapas.js
+++ www/js/mapas.js
@@ -1,42 +1,48 @@
-
 var mapOptions;
 var map;
 var marker;
 var watchID = null;
 var x;
 var y;
-			
+
 $(document).on('pageinit', '#page3', function(e, data)
 {    
-	var options = {maximumAge: 3000, timeout: 60000, enableHighAccuracy: true };
+	var options =
+	{
+		maximumAge: 3000,
+		timeout: 60000,
+		enableHighAccuracy: true
+	};
+	
     watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
 	
-	function onSuccess(position) {
+	function onSuccess(position)
+	{
         
         x = position.coords.latitude;
-        y =  position.coords.longitude;
+        y = position.coords.longitude;
         
         var center = new google.maps.LatLng(x, y);
 		mapOptions =
 		{
-			center:center,
-			zoom:12,
-			mapTypeId:google.maps.MapTypeId.ROADMAP,
-			draggable:true,
-			panControl:false,
-			zoomControl:true,
-			mapTypeControl:false,
-			scaleControl:false,
-			streetViewControl:false,
-			overviewMapControl:false,
+			center: center,
+			zoom: 12,
+			mapTypeId: google.maps.MapTypeId.ROADMAP,
+			draggable: true,
+			panControl: false,
+			zoomControl: true,
+			mapTypeControl: false,
+			scaleControl: false,
+			streetViewControl: false,
+			overviewMapControl: false,
 		};
 		
 		map = new google.maps.Map(document.getElementById('mapaperfil'), mapOptions);
 	
 		marker = new google.maps.Marker(
 		{
-			map:map,
-			draggable:false,
+			map: map,
+			draggable: false,
 			animation: google.maps.Animation.DROP,
 			position: center,
 			icon: 'img/moneda_takara.png'
@@ -46,8 +52,10 @@ $(document).on('pageinit', '#page3', function(e, data)
 
 	// onError Callback receives a PositionError object
 
-	function onError(error) {
-        alert('code: '    + error.code    + '\n' + 'message: ' + error.message + '\n');
+	function onError(error)
+	{
+		//$.mobile.changePage("dialog-boxes/error/error-gps.html", {role:"dialog"}); //<-- cargar con ajax
+        console.log('Código Error: '    + error.code    + '\n' + 'Mensaje: ' + error.message);
 	}
 });
   		 		
@@ -58,7 +66,3 @@ $('#page3').on('pageshow',function(event)
     map.setOptions(mapOptions); 
     map.setMarker(marker);
 });
-
-
-
-
