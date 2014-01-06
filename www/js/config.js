a855fce823212da40f25392dfcf3777d8126ae8f
Elolawyn <ral4576@gmail.com>
2014-01-06 09:52:54 +0100

    ty

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,6 @@
 //---------------------------------------------------------------- CONFIG ---------------------------------------------------------------*/
 $(document).ready(function()
 {
-	$("#ip-server").val("127.0.0.1");
-	
 	$(".botonconfig").click(function(e)
 	{
 		//$.mobile.changePage("dialog-boxes/info/sobre-app.html", {role:"dialog"}); //<-- cargar con ajax
